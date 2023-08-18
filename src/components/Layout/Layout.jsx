c2bc3adfa2610d3477347e133cb3d1052a744de1
OleksandrVaniuta <vaniutasa@gmail.com>
2023-08-18 10:42:21 +0300

    font-family

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import { Header } from '../Header/Header';
 
-import Container from 'components/Container/Container';
+// import Container from 'components/Container/Container';
 import Section from 'components/Section/Section';
 
 import { ContentWrapper } from './Layout.styled';
@@ -13,11 +13,11 @@ export const Layout = () => {
       <Header />
       <main>
         <Section>
-          <Container>
-            <Suspense fallback={null}>
-              <Outlet />
-            </Suspense>
-          </Container>
+          {/* <Container> */}
+          <Suspense fallback={null}>
+            <Outlet />
+          </Suspense>
+          {/* </Container> */}
         </Section>
       </main>
     </ContentWrapper>
