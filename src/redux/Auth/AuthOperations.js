72044980429d6f41ced0faa5cca6f5460d4502c1
OleksandrVaniuta <vaniutasa@gmail.com>
2023-08-18 11:48:09 +0300

    Stater Pack ready

---
+++ src/redux/Auth/AuthOperations.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+
+const setAuthHeader = token => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+const clearAuthHeader = () => {
+  axios.defaults.headers.common.Authorization = '';
+};
+
+export const register = createAsyncThunk('', async (credentials, thunkAPI) => {
+  try {
+    const res = await axios.post('', credentials);
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
+export const login = createAsyncThunk('', async (credentials, thunkAPI) => {
+  try {
+    const res = await axios.post('', credentials);
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
+export const logout = createAsyncThunk('', async (_, thunkAPI) => {
+  try {
+    await axios.post('');
+    clearAuthHeader();
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
+export const refresh = createAsyncThunk('', async (_, thunkAPI) => {
+  const state = thunkAPI.getState();
+  const persistedToken = state.auth.token;
+
+  if (persistedToken === null) {
+    return thunkAPI.rejectWithValue('Unable to fetch user');
+  }
+  try {
+    setAuthHeader(persistedToken);
+    const res = await axios.get('');
+    return res.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
