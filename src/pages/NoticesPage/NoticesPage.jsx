df6d5993239172fb74585ea858cf4b6a5a3abaca
Stanislav <Stas.zaliiski@gmail.com>
2023-08-23 17:49:28 +0200

    main

--- src/pages/NoticesPage/NoticesPage.jsx
+++ src/pages/NoticesPage/NoticesPage.jsx
@@ -1,18 +1,35 @@
+import axios from 'axios';
 import NotiesCategoriesNav from 'components/NoticesCategoriesNav/NotiesCategoriesNav';
 import NoticesSearch from 'components/NoticesSearch';
 import { PageTitle } from 'components/NoticesSearch/PageTitle.styled';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const NoticesPage = () => {
   // eslint-disable-next-line no-unused-vars
   const [page, setPage] = useState(1);
   // eslint-disable-next-line no-unused-vars
   const [search, setSearch] = useState('');
+  // eslint-disable-next-line no-unused-vars
+  const [notices, setNotices] = useState([]);
 
   const handleNoticeSearch = search => {
     setPage(1);
     setSearch(search);
   };
+
+  // отримую данні які додали на sell
+  useEffect(() => {
+    axios
+      .get(
+        'https://your-pet-backend-cmwy.onrender.com/api/notices/category/sell'
+      )
+      .then(response => {
+        setNotices(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching notices:', error);
+      });
+  }, []);
 
   return (
     <div>
