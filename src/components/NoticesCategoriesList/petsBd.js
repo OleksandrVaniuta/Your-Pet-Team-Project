b3148396276989206902ed7c4b8e66ce4a7108bc
YAlorenzo <zhenia.klimenchuk@gmail.com>
2023-08-23 19:57:13 +0200

    add_noties_List__fix_nav

--- src/components/NoticesCategoriesList/petsBd.js
+++ src/components/NoticesCategoriesList/petsBd.js
@@ -1,175 +1,3 @@
-import { pet1, pet10, pet11, pet12, pet2, pet3, pet4, pet5, pet6, pet7, pet8, pet9 } from "./img";
-
-export const PetList = [
-  {
-    category: 'In good hands',
-    id: '1',
-    img: pet1,
-    infoPet: {
-      city: 'Lviv',
-      age: '1 year',
-      sex: 'female',
-    },
-    name: 'Rich',
-    birthday: '20.09.2020',
-    type: 'Pomeranian',
-    title: 'Сute dog looking for a home',
-  },
-  {
-    category: 'In good hands',
-    id: '2',
-    img: pet2,
-    infoPet: {
-      city: 'Kyiv',
-      age: '4 year',
-      sex: 'female',
-    },
-    name: 'Cailo',
-    birthday: '21.03.2020',
-    type: 'Pomeranian',
-    title: 'Сute dog looking for a home',
-  },
-  {
-    category: 'In good hands',
-    id: '3',
-    img: pet3,
-    infoPet: {
-      city: 'Lviv',
-      age: '1 year',
-      sex: 'female',
-    },
-    name: 'Martin',
-    birthday: '10.05.2021',
-    type: 'Pomeranian',
-    title: 'Сute dog looking for a home',
-  },
-  {
-    category: 'In good hands',
-    id: '4',
-    img: pet4,
-    infoPet: {
-      city: 'Lviv',
-      age: '1 year',
-      sex: 'female',
-    },
-    name: 'Kailo',
-    birthday: '23.01.2020',
-    type: 'Pomeranian',
-    title: 'Сute dog looking for a home',
-  },
-  {
-    category: 'In good hands',
-    id: '5',
-    img: pet5,
-    infoPet: {
-      city: 'Lviv',
-      age: '2 year',
-      sex: 'female',
-    },
-    name: 'Lucia',
-    birthday: '20.09.2019',
-    type: 'Pomeranian',
-    title: 'Сute dog looking for a home',
-  },
-  {
-    category: 'In good hands',
-    id: '6',
-    img: pet6,
-    infoPet: {
-      city: 'Lviv',
-      age: '1 year',
-      sex: 'female',
-    },
-    name: 'Tor',
-    birthday: '20.09.2019',
-    type: 'Pomeranian',
-    title: 'Сute dog looking for a home',
-  },
-  {
-    category: 'In good hands',
-    id: '7',
-    img: pet7,
-    infoPet: {
-      city: 'Dnipro',
-      age: '1 year',
-      sex: 'female',
-    },
-    name: 'Anadel',
-    birthday: '20.09.2022',
-    type: 'Pomeranian',
-    title: 'Сute dog looking for a home',
-  },
-  {
-    category: 'In good hands',
-    id: '8',
-    img: pet8,
-    infoPet: {
-      city: 'Kyiv',
-      age: '7 year',
-      sex: 'female',
-    },
-    name: 'Koria',
-    birthday: '20.09.2020',
-    type: 'Pomeranian',
-    title: 'Сute dog looking for a home',
-  },
-  {
-    category: 'In good hands',
-    id: '9',
-    img: pet9,
-    infoPet: {
-      city: 'Lviv',
-      age: '2 year',
-      sex: 'female',
-    },
-    name: 'Nika',
-    birthday: '20.09.2030',
-    type: 'Pomeranian',
-    title: 'Сute dog looking for a home',
-  },
-  {
-    category: 'In good hands',
-    id: '10',
-    img: pet10,
-    infoPet: {
-      city: 'Kyiv',
-      age: '3 year',
-      sex: 'female',
-    },
-    name: 'Ulia',
-    birthday: '20.09.2019',
-    type: 'Pomeranian',
-    title: 'Сute dog looking for a home',
-  },
-  {
-    category: 'In good hands',
-    id: '11',
-    img: pet11,
-    infoPet: {
-      city: 'Lviv',
-      age: '1 year',
-      sex: 'female',
-    },
-    name: 'Santa',
-    birthday: '20.09.2019',
-    type: 'Pomeranian',
-    title: 'Сute dog looking for a home',
-  },
-  {
-    category: 'In good hands',
-    id: '12',
-    img: pet12,
-    infoPet: {
-      city: 'Kyiv',
-      age: '6 year',
-      sex: 'female',
-    },
-    name: 'Varia',
-    birthday: '20.09.2019',
-    type: 'Pomeranian',
-    title: 'Сute dog looking for a home',
-  },
-];
 
 export const categoriesNoLog = [
   { 
@@ -177,11 +5,11 @@ export const categoriesNoLog = [
     label: 'sell'
   },
   {
-    name: 'lost/found',
+    name: 'lost-found',
     label: 'lost/found'
   },
   {
-    name: 'in+good+hands',
+    name: 'in-good-hands',
     label: 'in good hands'
   },
 ];
@@ -191,19 +19,19 @@ export const categoriesLog = [
     label: 'sell'
   },
   {
-    name: 'lost/found',
+    name: 'lost-found',
     label: 'lost/found',
   },
   {
-    name: 'in+good+hands',
+    name: 'in-good-hands',
     label: 'in good hands',
   },
   {
-    name: 'favorite_ads',
+    name: 'favorite-ads',
     label: 'favorite ads',
   },
   {
-    name: 'my_ads',
+    name: 'my-ads',
     label: 'my ads',
   },
 ];
--- src/pages/NoticesPage/NoticesPage.jsx
+++ src/pages/NoticesPage/NoticesPage.jsx
@@ -1,24 +1,43 @@
+import axios from 'axios';
+import NoticesCategoriesList from 'components/NoticesCategoriesList/NoticesCategoriesList';
 import NotiesCategoriesNav from 'components/NoticesCategoriesNav/NotiesCategoriesNav';
 import NoticesSearch from 'components/NoticesSearch';
 import { PageTitle } from 'components/NoticesSearch/PageTitle.styled';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const NoticesPage = () => {
   // eslint-disable-next-line no-unused-vars
   const [page, setPage] = useState(1);
   // eslint-disable-next-line no-unused-vars
   const [search, setSearch] = useState('');
+  const [notices, setNotices] = useState([]);
+
+  const currentPath = window.location.pathname;
+  const category = currentPath.split('/').pop();
 
   const handleNoticeSearch = search => {
     setPage(1);
     setSearch(search);
   };
 
+  useEffect(() => {
+    axios
+      .get(`https://your-pet-backend-cmwy.onrender.com/api/notices/category/${category}`)
+      .then(response => {
+        setNotices(response.data);
+        
+      })
+      .catch(error => {
+        console.error('Error fetching notices:', error);
+      });
+  }, [category]);
+
   return (
     <div>
       <PageTitle>Find your favorite pet</PageTitle>
       <NoticesSearch handleSearch={handleNoticeSearch} />
-      <NotiesCategoriesNav></NotiesCategoriesNav>
+      <NotiesCategoriesNav />
+      <NoticesCategoriesList notice={notices}></NoticesCategoriesList>
     </div>
   );
 };
