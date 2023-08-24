72dc07d961dd9f8117be5acabb03026c5a581445
YAlorenzo <zhenia.klimenchuk@gmail.com>
2023-08-24 15:47:02 +0200

    fix_isLogNav

--- src/components/NoticesCategoriesNav/NotiesCategoriesNav.jsx
+++ src/components/NoticesCategoriesNav/NotiesCategoriesNav.jsx
@@ -3,12 +3,14 @@ import { categoriesLog, categoriesNoLog } from 'components/NoticesCategoriesList
 import { NavLink } from 'react-router-dom';
 import css from './NotiesCategoriesNav.module.css';
 import { selectIsLoggedIn } from 'redux/Auth/AuthSelectors';
+import { useSelector } from 'react-redux';
 
 function NotiesCategoriesNav() {
+  const isLoggedIn = useSelector(selectIsLoggedIn)
   return (
     <nav className={css.categoriesList_nav}>
       <ul className={css.categoriesList}>
-        {selectIsLoggedIn === true ? categoriesLog.map((category) => (
+        {isLoggedIn ? categoriesLog.map((category) => (
           <li key={category.name}>
              <NavLink to={`/notices/${category.name}`} className={css.list_item}>
               {category.label} 
