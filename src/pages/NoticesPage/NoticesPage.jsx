80e2ea0823c20499e503645bae9996b4ed342834
nikkuts <nickkuts888@gmail.com>
2023-08-28 22:58:40 +0300

    added pagination

--- src/pages/NoticesPage/NoticesPage.jsx
+++ src/pages/NoticesPage/NoticesPage.jsx
@@ -1,15 +1,13 @@
 import NoticesCategoriesList from 'components/NoticesCategoriesList/NoticesCategoriesList';
 import NotiesCategoriesNav from 'components/NoticesCategoriesNav/NotiesCategoriesNav';
 import NoticesSearch from 'components/NoticesSearch';
+import Pagination from 'components/Pagination/Pagination';
 import { PageTitle } from 'components/NoticesSearch/PageTitle.styled';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { fetchNoticesByCategory } from 'redux/notices/operations';
-import {
-  selectNotices,
-  // , selectTotalPages
-} from 'redux/notices/selectors';
+import { selectNotices } from 'redux/notices/selectors';
 
 export const NoticesPage = () => {
   const [page, setPage] = useState(1);
@@ -18,18 +16,18 @@ export const NoticesPage = () => {
   const dispatch = useDispatch();
   const notices = useSelector(selectNotices);
 
-  // хто робить пагінацію? це як отримати кількість сторінок загальну
-  // const totalPages = useSelector(selectTotalPages);
-  // console.log(totalPages);
-  // --------------------------------------------------------------------
-
   const params = useParams();
   const category = params.category;
 
   const handleNoticeSearch = search => {
-    setPage(1);
     setSearch(search);
   };
+
+  const handlePagination = page => setPage(page);
+
+  useEffect(() => {
+    setPage(1);
+  }, [category, search]);
 
   useEffect(() => {
     dispatch(
@@ -48,6 +46,11 @@ export const NoticesPage = () => {
       <NoticesSearch handleSearch={handleNoticeSearch} />
       <NotiesCategoriesNav />
       <NoticesCategoriesList notice={notices}></NoticesCategoriesList>
+      <Pagination 
+      handlePagination={handlePagination}
+      category={category}
+      search={search}
+      />
     </div>
   );
 };
