7bdfea6f835c6829bc390ba350b4bb3b29b4e1d7
Olga-Dames <113420629+Olga-Dames@users.noreply.github.com>
2023-08-28 22:20:15 +0300

    Update LoginForm.jsx

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router';
 import { useDispatch } from 'react-redux';
 import { Formik } from 'formik';
