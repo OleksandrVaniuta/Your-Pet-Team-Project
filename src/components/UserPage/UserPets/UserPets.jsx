2d8c06ea46783a680bed8a295ca78fd60296295f
OleksandrVaniuta <vaniutasa@gmail.com>
2023-08-30 00:58:43 +0300

    sorry Olga

--- src/components/UserPage/UserPets/UserPets.jsx
+++ src/components/UserPage/UserPets/UserPets.jsx
@@ -4,15 +4,28 @@ import { selectMyPetsData } from '../../../redux/Profile/ProfileSelectors';
 import UserPetsHeader from './UserPetsHeader/UserPetsHeader';
 import PetsList from './UserPetsList/UserPetsList';
 
-import { UserPetsCOntainer } from './UserPets.styled';
+import {
+  UserPetsCOntainer,
+  NoPetsMessageWrapper,
+  NoPetParagraph,
+} from './UserPets.styled';
 
 export default function UserPets() {
   const pets = useSelector(selectMyPetsData);
 
   return (
     <UserPetsCOntainer>
-      <UserPetsHeader />
       {pets && <PetsList pets={pets} />}
+      <UserPetsHeader />
+      {!pets.length && (
+        <NoPetsMessageWrapper>
+          <NoPetParagraph>
+            You haven't added any of your beloved pets yet.
+          </NoPetParagraph>
+          <p>To add it, click the 'Add pet' button above.</p>
+        </NoPetsMessageWrapper>
+      )}
+      {pets.length > 0 && <PetsList pets={pets} />}
     </UserPetsCOntainer>
   );
 }
