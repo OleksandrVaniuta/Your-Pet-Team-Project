5dbc7cb5560a5c8a1045340ac91f0834698b82fe
IrynaBoryna <borisov_ka@ukr.net>
2023-08-30 17:58:04 +0300

    оновлення

--- src/pages/AddPetPage/AddPetSteps/ChooseOption/chooseOption.styled.jsx
+++ src/pages/AddPetPage/AddPetSteps/ChooseOption/chooseOption.styled.jsx
@@ -1,19 +1,16 @@
 import styled from '@emotion/styled';
 
-export const Container = styled.div``;
-
 export const CategoryForm = styled.ul`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  width: 131px;
-  height: 176px;
   gap: 12px;
+  margin-left: 8px;
   margin-bottom: 95px;
   margin-top: 24px;
   @media screen and (min-width: 767px) {
     margin-bottom: 137px;
-    margin-top: 40px;
+    margin-top: 20px;
   }
 `;
 
@@ -33,13 +30,17 @@ export const CheckedLabel = styled.label`
 
  background-color: ${props => (props.checked ? '#54adff' : '#CCE4FB')};
   color: ${props => (props.checked ? '#FEF9F9' : '#54ADFF')};
+   :hover {
+    transition: all 0.2s ease-in-out;
+    transform: scale(1.05);
+  }
 
   > input[type='radio'] {
     // visibility: hidden;
+      border-radius: 40px;
     height: 0;
     width: 0;
   }
 
   }
 `;
-
--- src/pages/AddPetPage/Styles/addPetPage.styled.jsx
+++ src/pages/AddPetPage/Styles/addPetPage.styled.jsx
@@ -3,32 +3,26 @@ import styled from '@emotion/styled';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
-
-  max-width: 458px;
-  margin: auto;
   padding: 20px 8px 17px 8px;
+  justify-content: center;
+  max-width: 280px;
   border-radius: 40px;
-
-  border: none;
   background-color: #fff;
-  boxshadows: {
-    main: '3px 8px 14px rgba(136, 198, 253, 0.19)';
-  }
-  ,
+  boxshadows: '3px 8px 14px rgba(136, 198, 253, 0.19)';
+
   @media screen and (min-width: 767px) {
-    padding: 20px 32px;
+    margin: auto;
+    padding: 20px 32px 20px 32px;
     max-width: ${props =>
-      props.category !== 'my-pet' && props.step === 2 ? '704px' : '458px'};
+      props.category !== 'your pet' && props.step === 2 ? '704px' : '458px'};
   }
   @media screen and (min-width: 1280px) {
     max-width: ${props =>
-      props.category !== 'my-pet' && props.step === 2 ? '822px' : '458px'};
+      props.category !== 'your pet' && props.step === 2 ? '822px' : '458px'};
     padding: ${props =>
       props.category !== 'my-pet' && props.step === 2
         ? '30px 75px'
         : '20px 32px'};
   }
 `;
-export const Dog = styled.svg``;
-export const Back = styled.svg``;
+
